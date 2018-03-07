7c3ddc630ffc4386497fe4831457f5e88adc4520
Dmitriy Shmatov <d.shmatov@anchorfree.com>
2018-03-07 19:42:17 +0200

    Add wait for ES

--- bin/run.sh
+++ bin/run.sh
@@ -9,6 +9,16 @@ if [[ -f ${TMP_CONFIG} ]]; then
     envtpl < /tmp/kibana.tpl > ${KIBANA_HOME_DIR}/config/kibana.yml
 fi
 
+check=true
+while ${check}
+do
+    sleep 5
+    curl --silent --show-error --fail --fail-early --connect-timeout 5 ${ELASTICSEARCH_HOST}:${ELASTICSEARCH_PORT}
+    if [ "$?" -eq 0 ]; then
+        check=false
+    fi
+done
+
 # Create kibana index
 if [[ $(curl --write-out %{http_code} --silent --output /dev/null "${ELASTICSEARCH_HOST}:${ELASTICSEARCH_PORT}/${KIBANA_INDEX_NAME}") -eq 404 ]]; then
   curl -XPUT "${ELASTICSEARCH_HOST}:${ELASTICSEARCH_PORT}/${KIBANA_INDEX_NAME}" -d'{"settings" :{"index" : {"number_of_shards" : ${ELASTICSEARCH_SHARDS}, "number_of_replicas" : ${ELASTICSEARCH_REPLICAS}}}}'
@@ -17,4 +27,4 @@ fi
 curl "http://${ELASTICSEARCH_HOST}:${ELASTICSEARCH_PORT}/"
 
 # Run kibana
-${KIBANA_HOME_DIR}/bin/kibana
+exec ${KIBANA_HOME_DIR}/bin/kibana
