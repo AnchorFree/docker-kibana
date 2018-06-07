40b5d0360b749933a1824a7be2bf3d60d28983cd
Dmitriy Shmatov <d.shmatov@anchorfree.com>
2018-06-07 11:35:23 +0300

    Proper exception handling, Get Kibana port from env

--- tests/block-search.js
+++ tests/block-search.js
@@ -3,19 +3,25 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch({executablePath: '/usr/bin/chromium-browser', args: ['--no-sandbox']});
-  const page = await browser.newPage();
+  try {
+    const kibanaPort = process.env.SERVER_PORT || "5601"
+    const browser = await puppeteer.launch({executablePath: '/usr/bin/chromium-browser', args: ['--no-sandbox']});
+    const page = await browser.newPage();
 
-  await page.goto('http://127.0.0.1:5601');
+    await page.goto('http://127.0.0.1:' + kibanaPort);
 
-  await page.waitFor(10000);
+    await page.waitFor(10000);
 
-  const result = await page.evaluate(() => {
-    let links = document.querySelector('div.global-nav__links').innerText;
-    return links.includes('Discover');
-  });
+    const result = await page.evaluate(() => {
+      let links = document.querySelector('div.global-nav__links').innerText;
+      return links.includes('Discover');
+    });
 
-  await browser.close();
+    await browser.close();
 
-  process.exit(!result)
+    process.exit(!result);
+  } catch(e) {
+    console.log(e);
+    process.exit(1);
+  }
 })();
